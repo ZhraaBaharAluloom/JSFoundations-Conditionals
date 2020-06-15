aa4050e92b7909e90e9b53a91189724709413539
ZhraaBaharAluloom <alwarda@hotmail.co.uk>
2020-06-15 20:02:39 +0300

    calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
 // Define the numbers and the operation here
+const two= 2;
+const four= 4;
+const calculator= "+"
+
 
 // Write if-statements here
+if (calculator== "+") {
+    console.log (two+four);
+}
+else if (calculator=== "*") {
+    console.log (two*four);
+}
+else if (calculator=== "-") {
+    console.log (two-four);
+}
+else if (calculator=== "/") {
+    console.log (two/four);
+}
+else {
+    console.log("Nothing ^-^");
+}
