5662f7e9932059aa4286baad2f8aa574ac2d5389
ZhraaBaharAluloom <alwarda@hotmail.co.uk>
2020-06-15 19:55:08 +0300

    Calculator.js

---
+++ Calculation.js
@@ -0,0 +1,19 @@
+const two= 2;
+const four= 4;
+const calculator= "+"
+
+if (calculator== "+") {
+    console.log (two+four);
+}
+else if (calculator=== "*") {
+    console.log (two*four);
+}
+else if (calculator=== "-") {
+    console.log (two-four);
+}
+else if (calculator=== "/") {
+    console.log (two/four);
+}
+else {
+    console.log("Nothing ^-^");
+}
